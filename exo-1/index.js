2f7821086a020466bd2068567561b23f3b4a0c76
codwerk <louis_widdy@live.fr>
2020-04-22 19:06:54 +0200

    exo champs de formulaire

---
+++ exo-1/index.js
@@ -0,0 +1,30 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    valeur: '',
+    Names: [],
+    infos: [{
+      value: "FR",
+      intitule: "francaise"
+    },
+    {
+      value: "BL",
+      intitule: "belge"
+    }],
+    abos: [{
+      value: "abonewspromo",
+      sentences: "M'abonner à la newsletter et aux promotions"
+    },
+    {
+      value: "abonews",
+      sentences: "M'abonner uniquement à la newsletter"
+    },
+    {
+      value: "aborien",
+      sentences: "Ne pas m'abonner"
+    }],
+    methods: {
+
+    }
+  }
+})
